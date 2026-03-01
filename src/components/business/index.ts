30e172cd528de16afd1c65522b3a04f39d9a971d
greenisi <120649289+greenisi@users.noreply.github.com>
2026-02-28 22:19:54 -0500

    Create index.ts

---
+++ src/components/business/index.ts
@@ -0,0 +1,9 @@
+// Business Content Management Components
+// Barrel export file for all business-type components
+
+export { ServicesList } from './ServicesList';
+export { ProductsList } from './ProductsList';
+export { PropertiesList } from './PropertiesList';
+export { PropertyDetail } from './PropertyDetail';
+export { ShoppingCart } from './ShoppingCart';
+export { CheckoutPage } from './CheckoutPage';
